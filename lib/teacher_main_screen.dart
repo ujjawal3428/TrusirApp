947fb1ca158bd9e85c0f1e32864b66e580f1c7ca
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2024-12-07 17:39:18 +0530

    first

--- lib/teacher_main_screen.dart
+++ lib/teacher_main_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:trusir/bottom_navigation_bar.dart';
-import 'package:trusir/course.dart';
-import 'package:trusir/setting.dart';
 import 'package:trusir/teacher_facilities.dart';
+import 'package:trusir/teacherssettings.dart';
 
 class TeacherMainScreen extends StatefulWidget {
   const TeacherMainScreen({super.key});
@@ -16,9 +15,8 @@ class TeacherMainScreenState extends State<TeacherMainScreen> {
 
   // List of pages for each bottom navigation item
   final List<Widget> pages = [
-    const TeacherFacilities(), // Home page (Student Facilities)
-    const CoursePage(), // Placeholder for Courses
-    const SettingsScreen(), // Placeholder for Menu
+    const TeacherFacilities(), // Home page (Student Facilities) // Placeholder for Courses
+    const Teacherssettings(), // Placeholder for Menu
   ];
 
   // Function to handle bottom navigation item taps
