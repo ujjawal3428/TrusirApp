dae235164aa0b84c487538f98bd2c5c6a3528c3f
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2024-12-07 23:19:07 +0530

    first

--- lib/addtestseries.dart
+++ lib/addtestseries.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+import 'package:trusir/api.dart';
 
 class Addtestseries extends StatefulWidget {
   const Addtestseries({super.key});
@@ -10,34 +16,138 @@ class Addtestseries extends StatefulWidget {
 class _AddtestseriesState extends State<Addtestseries> {
   final _formKey = GlobalKey<FormState>();
   String? selectedSubject;
-  DateTime? selectedDate;
-  TimeOfDay? selectedTime;
+  final TextEditingController _testNameController = TextEditingController();
+  String? photo;
+  String? answer;
+  String? question;
 
-  final List<String> subjects = ['Mathematics', 'Science', 'English', 'History'];
+  Future<String?> _getUserID() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('userID'); // Retrieve 'userID' from storage
+  }
 
-  void _pickDate() async {
-    DateTime? date = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (date != null) {
-      setState(() {
-        selectedDate = date;
-      });
+  Future<String> uploadImage(XFile imageFile) async {
+    final uri = Uri.parse('$baseUrl/api/upload-profile');
+    final request = http.MultipartRequest('POST', uri);
+
+    // Add the image file to the request
+    request.files
+        .add(await http.MultipartFile.fromPath('photo', imageFile.path));
+
+    // Send the request
+    final response = await request.send();
+
+    if (response.statusCode == 201) {
+      // Parse the response to extract the download URL
+      final responseBody = await response.stream.bytesToString();
+      final Map<String, dynamic> jsonResponse = jsonDecode(responseBody);
+
+      if (jsonResponse.containsKey('download_url')) {
+        return jsonResponse['download_url'] as String;
+      } else {
+        print('Download URL not found in the response.');
+        return 'null';
+      }
+    } else {
+      print('Failed to upload image: ${response.statusCode}');
+      return 'null';
     }
   }
 
-  void _pickTime() async {
-    TimeOfDay? time = await showTimePicker(
-      context: context,
-      initialTime: TimeOfDay.now(),
-    );
-    if (time != null) {
-      setState(() {
-        selectedTime = time;
-      });
+  Future<void> handleImageSelection(String? path, bool quest) async {
+    try {
+      final ImagePicker picker = ImagePicker();
+      final XFile? pickedFile =
+          await picker.pickImage(source: ImageSource.gallery);
+
+      if (pickedFile != null) {
+        // Upload the image and get the path
+        final uploadedPath = await uploadImage(pickedFile);
+        if (uploadedPath != 'null') {
+          setState(() {
+            // Example: Update the first student's photo path
+            if (quest) {
+              question = uploadedPath;
+            } else {
+              answer = uploadedPath;
+            }
+            //)
+          });
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Image Uploaded Successfully!'),
+              duration: Duration(seconds: 1),
+            ),
+          );
+          print('Image uploaded successfully: $uploadedPath');
+        } else {
+          print('Failed to upload the image.');
+        }
+      } else {
+        print('No image selected.');
+      }
+    } catch (e) {
+      print('Error during image selection: $e');
+    }
+  }
+
+  final List<String> subjects = [
+    'Mathematics',
+    'Science',
+    'English',
+    'History'
+  ];
+
+  // Function to send data to the API
+  Future<void> _sendTestData(String testName, String subject) async {
+    // Get the current date and time
+    final now = DateTime.now();
+    final currentDate = "${now.year}-${now.month}-${now.day}";
+    final currentTime = "${now.hour}:${now.minute}";
+
+    final userID = await _getUserID();
+    if (userID == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('User ID not found. Please log in again.')),
+      );
+      return;
+    }
+
+    // Construct the API payload
+    final postData = {
+      "userID": userID, // Replace with dynamic user ID if available
+      "test_name": testName,
+      "question": question,
+      "answer": answer,
+      "subject": subject,
+      "date": currentDate,
+      "time": currentTime,
+    };
+
+    // Send the POST request
+    const apiUrl =
+        "$baseUrl/api/store-test-series"; // Replace with your API URL
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {"Content-Type": "application/json"},
+        body: json.encode(postData),
+      );
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Test uploaded successfully!')),
+        );
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Failed to upload test.')),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Error: $e')),
+      );
     }
   }
 
@@ -86,6 +196,7 @@ class _AddtestseriesState extends State<Addtestseries> {
             children: [
               // Test Name
               TextFormField(
+                controller: _testNameController,
                 decoration: InputDecoration(
                   labelText: 'Test Name',
                   border: OutlineInputBorder(
@@ -102,7 +213,6 @@ class _AddtestseriesState extends State<Addtestseries> {
                 },
               ),
               const SizedBox(height: 16),
-
               // Subject Dropdown
               DropdownButtonFormField<String>(
                 value: selectedSubject,
@@ -134,77 +244,42 @@ class _AddtestseriesState extends State<Addtestseries> {
               ),
               const SizedBox(height: 16),
 
-              // Date Picker
-              InkWell(
-                onTap: _pickDate,
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    labelText: 'Select Date',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  child: Text(
-                    selectedDate != null
-                        ? '${selectedDate!.day}-${selectedDate!.month}-${selectedDate!.year}'
-                        : 'No date selected',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
-              // Time Picker
-              InkWell(
-                onTap: _pickTime,
-                child: InputDecorator(
-                  decoration: InputDecoration(
-                    labelText: 'Select Time',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                  child: Text(
-                    selectedTime != null
-                        ? '${selectedTime!.hour}:${selectedTime!.minute} ${selectedTime!.period == DayPeriod.am ? 'AM' : 'PM'}'
-                        : 'No time selected',
-                    style: const TextStyle(fontSize: 16),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  SizedBox(
-                    height: 40,
-                    width: 150,
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        // Here, implement the file picker logic for questions
-                      },
-                      icon: const Icon(Icons.upload_file),
-                      label: const Text('Upload Questions', style: 
-                      TextStyle(fontSize: 12),),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 40,
-                    width: 150,
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        // Here, implement the file picker logic for answers
-                      },
-                      icon: const Icon(Icons.upload_file),
-                      label: const Text('Upload Answers',style: 
-                      TextStyle(fontSize: 12),),
-                    ),
-                  ),
+                  question != null
+                      ? Image.network(width: 50, height: 50, question!)
+                      : SizedBox(
+                          height: 40,
+                          width: 150,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              handleImageSelection(question,
+                                  true); // Here, implement the file picker logic for questions
+                            },
+                            icon: const Icon(Icons.upload_file),
+                            label: const Text(
+                              'Upload Questions',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ),
+                        ),
+                  answer != null
+                      ? Image.network(width: 50, height: 50, answer!)
+                      : SizedBox(
+                          height: 40,
+                          width: 150,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              handleImageSelection(answer, false);
+                            },
+                            icon: const Icon(Icons.upload_file),
+                            label: const Text(
+                              'Upload Answers',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                          ),
+                        ),
                 ],
               ),
               const SizedBox(height: 16),
@@ -213,10 +288,11 @@ class _AddtestseriesState extends State<Addtestseries> {
               ElevatedButton(
                 onPressed: () {
                   if (_formKey.currentState!.validate()) {
-                    // Handle form submission (No API integration)
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Test uploaded successfully!')),
+                    _sendTestData(
+                      _testNameController.text,
+                      selectedSubject!,
                     );
+                    Navigator.pop(context);
                   }
                 },
                 style: ElevatedButton.styleFrom(
