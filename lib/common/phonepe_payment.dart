72ab72ae1c29b82403c0af94ee5c3f4bae4ffc2c
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2024-12-29 13:57:56 +0530

    first commit

--- lib/common/phonepe_payment.dart
+++ lib/common/phonepe_payment.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:crypto/crypto.dart';
+
 import 'package:phonepe_payment_sdk/phonepe_payment_sdk.dart';
+import 'package:flutter/material.dart';
 
 class PaymentPage extends StatefulWidget {
   const PaymentPage({super.key});
@@ -18,10 +22,17 @@ class PaymentPageState extends State<PaymentPage> {
   String checksum = ""; // Obtain this from your backend
   String apiEndPoint = "/pg/v1/pay";
   String callback = "TrusirApp";
+  String saltKey = "96434309-7796-489d-8924-ab56988a6076";
+  // String merchantId = "M1U6UCYTTPBT";
+  //  String environmentValue =
+  //     'PRODUCTION';
+  // String saltKey = "77c97305-4c07-4586-829a-03767fea9e64";
+  String saltIndex = "1";
 
   @override
   void initState() {
     super.initState();
+    body = getChecksum().toString();
     initPhonePeSdk();
   }
 
@@ -54,16 +65,19 @@ class PaymentPageState extends State<PaymentPage> {
 
   getChecksum() {
     final reqData = {
-      {
-        "merchantId": merchantId,
-        "merchantTransactionId": "t_52554",
-        "merchantUserId": "MUID123",
-        "amount": 10000,
-        "callbackUrl": callback,
-        "mobileNumber": "9999999999",
-        "paymentInstrument": {"type": "PAY_PAGE"}
-      }
+      "merchantId": merchantId,
+      "merchantTransactionId": "t_52554",
+      "merchantUserId": "MUID123",
+      "amount": 10,
+      "callbackUrl": callback,
+      "mobileNumber": "9999999999",
+      "paymentInstrument": {"type": "PAY_PAGE"}
     };
+    String base64body = base64.encode(utf8.encode(json.encode(reqData)));
+    checksum =
+        '${sha256.convert(utf8.encode(base64body + apiEndPoint + saltKey)).toString()}###$saltIndex';
+
+    return base64body;
   }
 
   @override
