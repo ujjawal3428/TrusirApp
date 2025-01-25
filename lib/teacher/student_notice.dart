c88652e094b9f84df7fef1fc5b101bd974eddb96
AnshGit-hash <73894697+AnshGit-hash@users.noreply.github.com>
2025-01-25 16:32:45 +0530

    first commit

--- lib/teacher/student_notice.dart
+++ lib/teacher/student_notice.dart
@@ -89,177 +89,181 @@ class _StudentNoticeScreenState extends State<StudentNoticeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[50],
-      appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
         backgroundColor: Colors.grey[50],
-        elevation: 0,
-        automaticallyImplyLeading: false,
-        title: Padding(
-          padding: const EdgeInsets.only(left: 1.0),
-          child: Row(
-            children: [
-              GestureDetector(
-                  onTap: () {
-                    Navigator.pop(context);
-                  },
-                  child: Image.asset('assets/back_button.png', height: 50)),
-              const SizedBox(width: 20),
-              const Text(
-                'Notice',
-                style: TextStyle(
-                  color: Color(0xFF48116A),
-                  fontSize: 25,
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w700,
+        appBar: AppBar(
+          backgroundColor: Colors.grey[50],
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 1.0),
+            child: Row(
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Image.asset('assets/back_button.png', height: 50)),
+                const SizedBox(width: 20),
+                const Text(
+                  'Notice',
+                  style: TextStyle(
+                    color: Color(0xFF48116A),
+                    fontSize: 25,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
+          toolbarHeight: 70,
         ),
-        toolbarHeight: 70,
-      ),
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        child: Stack(
-          children: [
-            isLoading
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : notices.isEmpty
-                    ? Center(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "No Notices Available",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.w500,
-                                color: Colors.grey,
-                              ),
-                            ),
-                            _buildAddNoticeButton(context),
-                          ],
-                        ),
-                      )
-                    : SingleChildScrollView(
-                        child: Padding(
-                          padding: const EdgeInsets.only(
-                            left: 15,
-                            right: 15,
-                            bottom: 15,
-                            top: 0,
-                          ),
+        body: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              isLoading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : notices.isEmpty
+                      ? Center(
                           child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              ...notices.asMap().entries.map((entry) {
-                                int index = entry.key;
-                                Notice notice = entry.value;
-
-                                // Cycle through colors using the modulus operator
-                                Color cardColor =
-                                    cardColors[index % cardColors.length];
-
-                                return Padding(
-                                  padding: const EdgeInsets.only(
-                                    left: 10,
-                                    top: 20,
-                                    right: 10,
-                                  ),
-                                  child: Stack(
-                                    alignment: Alignment.center,
-                                    children: [
-                                      Container(
-                                        width: 386,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          color: cardColor,
-                                        ),
-                                        child: Padding(
-                                          padding: const EdgeInsets.only(
-                                              left: 55, top: 10, bottom: 10),
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              Text(
-                                                notice.noticetitle,
-                                                style: const TextStyle(
-                                                  fontSize: 16,
-                                                  fontWeight: FontWeight.w600,
-                                                  color: Colors.black,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 5),
-                                              Text(
-                                                'Posted on : ${notice.date}',
-                                                style: const TextStyle(
-                                                  fontSize: 13,
-                                                  fontWeight: FontWeight.w400,
-                                                  color: Colors.grey,
-                                                ),
-                                              ),
-                                              const SizedBox(height: 5),
-                                              Text(
-                                                notice.notice,
-                                                style: const TextStyle(
-                                                  fontSize: 13,
-                                                  fontWeight: FontWeight.w500,
-                                                  color: Colors.black,
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      Positioned(
-                                        top: 10,
-                                        left: 10,
-                                        child: Image.asset(
-                                          'assets/bell.png',
-                                          width: 36,
-                                          height: 36,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              }),
-                              hasMore
-                                  ? Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          vertical: 10),
-                                      child: isLoadingMore
-                                          ? const CircularProgressIndicator()
-                                          : TextButton(
-                                              onPressed: () {
-                                                setState(() {
-                                                  isLoadingMore = true;
-                                                  currentPage++;
-                                                });
-                                                fetchNotices(page: currentPage);
-                                              },
-                                              child: const Text('Load More...'),
-                                            ),
-                                    )
-                                  : const Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 20),
-                                      child: Text('No more Notices'),
-                                    ),
+                              const Text(
+                                "No Notices Available",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              _buildAddNoticeButton(context),
                             ],
                           ),
+                        )
+                      : SingleChildScrollView(
+                          child: Padding(
+                            padding: const EdgeInsets.only(
+                              left: 15,
+                              right: 15,
+                              bottom: 15,
+                              top: 0,
+                            ),
+                            child: Column(
+                              children: [
+                                ...notices.asMap().entries.map((entry) {
+                                  int index = entry.key;
+                                  Notice notice = entry.value;
+
+                                  // Cycle through colors using the modulus operator
+                                  Color cardColor =
+                                      cardColors[index % cardColors.length];
+
+                                  return Padding(
+                                    padding: const EdgeInsets.only(
+                                      left: 10,
+                                      top: 20,
+                                      right: 10,
+                                    ),
+                                    child: Stack(
+                                      alignment: Alignment.center,
+                                      children: [
+                                        Container(
+                                          width: 386,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            color: cardColor,
+                                          ),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 55, top: 10, bottom: 10),
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Text(
+                                                  notice.noticetitle,
+                                                  style: const TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.w600,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 5),
+                                                Text(
+                                                  'Posted on : ${notice.date}',
+                                                  style: const TextStyle(
+                                                    fontSize: 13,
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Colors.grey,
+                                                  ),
+                                                ),
+                                                const SizedBox(height: 5),
+                                                Text(
+                                                  notice.notice,
+                                                  style: const TextStyle(
+                                                    fontSize: 13,
+                                                    fontWeight: FontWeight.w500,
+                                                    color: Colors.black,
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                        ),
+                                        Positioned(
+                                          top: 10,
+                                          left: 10,
+                                          child: Image.asset(
+                                            'assets/bell.png',
+                                            width: 36,
+                                            height: 36,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                }),
+                                hasMore
+                                    ? Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 10),
+                                        child: isLoadingMore
+                                            ? const CircularProgressIndicator()
+                                            : TextButton(
+                                                onPressed: () {
+                                                  setState(() {
+                                                    isLoadingMore = true;
+                                                    currentPage++;
+                                                  });
+                                                  fetchNotices(
+                                                      page: currentPage);
+                                                },
+                                                child:
+                                                    const Text('Load More...'),
+                                              ),
+                                      )
+                                    : const Padding(
+                                        padding:
+                                            EdgeInsets.symmetric(vertical: 20),
+                                        child: Text('No more Notices'),
+                                      ),
+                              ],
+                            ),
+                          ),
                         ),
-                      ),
-            Positioned(
-                left: 0,
-                right: 0,
-                bottom: 30,
-                child: _buildAddNoticeButton(context)),
-          ],
+              Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: 30,
+                  child: _buildAddNoticeButton(context)),
+            ],
+          ),
         ),
       ),
     );
